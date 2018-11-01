1222e0fbf90b0f034fa36ce69b15859302652034
Vijay Archakam <vijay.ananthan286@gmail.com>
2018-11-01 21:58:04 +0530

    Commit_apply

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 yum update –y
 yum install java-1.8.0-openjdk-devel.x86_64 -y
+yum install git
 wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
 yum install -y apache-maven
